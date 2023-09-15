b34c421e850ecdf5671c724f7925040fde093d1b
Sal <81916015+Saphal47@users.noreply.github.com>
2023-09-15 12:35:10 +0530

    Update WhatGPT3.jsx

--- gpt/src/containers/whatGPT3/WhatGPT3.jsx
+++ gpt/src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,7 +6,8 @@ const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id='wgpt3'>
       <div className="gpt3__whatgpt3-feature">
-        <Feature title="What is GPT3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
+        <Feature title="What is GPT3" 
+                  text="GPT-3, short for Generative Pre-trained Transformer 3, is a cutting-edge natural language processing (NLP) model developed by OpenAI. It is the third iteration of the GPT series and represents one of the most advanced AI language models available as of my last knowledge update in September 2021."/>
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">
@@ -17,9 +18,9 @@ const WhatGPT3 = () => {
 
       {/* //for cards below */}
       <div className="gpt3__whatgpt3-container">
-        <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "/>
-        <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments "/>
-        <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"/>
+        <Feature title="Generative Model" text=" it can generate text, answer questions, translate languages, summarize documents, and perform various other language-related tasks by predicting and generating text based on the input it receives. "/>
+        <Feature title="Multilingual Capability" text="GPT-3 has demonstrated the ability to work with multiple languages and perform translation tasks between them. Its versatility makes it a valuable tool for cross-lingual applications."/>
+        <Feature title="Massive Scale" text="It's a massive neural network with 175 billion parameters, making it significantly larger and more powerful than its predecessors. This vast scale allows it to generate human-like text and comprehend a wide range of natural language tasks."/>
       </div>
     </div>
 
