3792d94854eb27e0272eb44de22342f909e80451
Sal <81916015+Saphal47@users.noreply.github.com>
2023-09-15 12:31:24 +0530

    add route

--- gpt/src/components/navbar/Navbar.jsx
+++ gpt/src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 import './navbar.css';
+import { BrowserRouter,Link } from 'react-router-dom';
 
 //This is fxn component which will render all the navLinks
 //Caution: used ()=> () not ()=> {}
@@ -29,10 +30,12 @@ const Navbar = () => {
           <Menu/>
         </div>
       </div>
-      <div className="gpt3__navbar-sign">
-        <p>Sign in</p>
-        <button type="button">Sign up</button>
-      </div>
+      <BrowserRouter>
+        <div className="gpt3__navbar-sign">
+          <Link to="https://auth0.openai.com/u/login/identifier?state=hKFo2SBYRW5tNmx6MVdHMk5RMjhVLWJLWGNfcTltMjRqQ1ZXUqFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIE5kNnFSWXJIN2pYWVAyckROdjVtQ3ItdzdMeGdBN0d4o2NpZNkgRFJpdnNubTJNdTQyVDNLT3BxZHR3QjNOWXZpSFl6d0Q"> <p>Sign in</p>  </Link>
+          <Link to="https://auth0.openai.com/u/signup/identifier?state=hKFo2SBwSGs5QW00aTJ4cS11OEZlZlZYUlR1RU84aVFvTmhHZKFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIEhkNy04cDVGeXV6VjVuV3hCMVhXUGVLX0FiRVowYjdio2NpZNkgRFJpdnNubTJNdTQyVDNLT3BxZHR3QjNOWXZpSFl6d0Q"> <button type="button">Sign up</button> </Link>
+        </div>
+      </BrowserRouter>
       
       {/* For Mobile version */}
       <div className="gpt3__navbar-menu">
