c2e0454be6d7e755858bdf842eb59d550c972ce0
Sal <81916015+Saphal47@users.noreply.github.com>
2023-09-15 12:30:49 +0530

    add route

--- gpt/src/components/cta/CTA.jsx
+++ gpt/src/components/cta/CTA.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter,Link } from 'react-router-dom';
 import './cta.css';
 const CTA = () => {
   return (
@@ -7,9 +8,13 @@ const CTA = () => {
         <p>Request Early Access to Get Started</p>
         <h3>Register today & start exploring the endless possiblities.</h3>
       </div>
+      <BrowserRouter>
       <div className="gpt3__cta-btn">
+        <Link to="https://openai.com/">
         <button>Get Started</button>
+        </Link>
       </div>
+      </BrowserRouter>
     </div>
   )
 }
